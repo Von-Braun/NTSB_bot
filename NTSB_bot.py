ef5169144a42838c0660b2393006352c71c56f9a
Greg Macleod <gerardkoufax@gmail.com>
2017-01-24 20:28:42 -0500

    Update NTSB_bot.py

--- NTSB_bot.py
+++ NTSB_bot.py
@@ -61,16 +61,13 @@ def post_incident(data,catagories):
     soup.prettify()
 
     for anchor in soup.findAll('span', {"id": "ctl00_PlaceHolderMain_lblNarrAcc"}):
-        print anchor.contents,anchor
         for a in anchor:
             if (a.string is None):
                 a.string = '\n'
-        print 'printing anchor text'
         all_text=filter(lambda x: x in printable, anchor.get_text()) 
         body_text = all_text.decode('utf-8').encode("ascii","ignore")
 
         if body_text=='\n':
-            print 'No text'
             page = requests.get(str('https://www.ntsb.gov/_layouts/ntsb.aviation/brief.aspx?ev_id='+data[0]).strip(), verify=False).text[:]
             soup = BeautifulSoup(page, "lxml")
             soup.prettify()
@@ -78,7 +75,7 @@ def post_incident(data,catagories):
                 all_text=filter(lambda x: x in printable, backup.get_text())
                 body_text = all_text.encode("ascii","ignore")
 
-        print 'appending'
+        print 'Appending'
         text_body.append(body_text.decode('utf-8').encode("ascii","ignore"))
     text_body.append(('\n\n|Category|Data|Category|Data|Category|Data|\n|-----------:|:------------|-----------:|:------------|-----------:|:------------\n').decode('utf-8').encode("ascii","ignore"))
     for i in catagories: #go over each catagory and print data
@@ -100,7 +97,7 @@ def post_incident(data,catagories):
     title=filter(lambda x: x in printable, title)
     post_id = subreddit.submit(title=title.decode('utf-8').encode("ascii","ignore"), selftext=''.join(text_body)) #command,PostID,DecryptionID
     save_post_id(post_id.id)
-    if verbose: print '        Post Submitted succesfully'
+    if verbose: print '        Post Submitted succesfully\n'
 
 login_file=open('login.txt','r')
 REDDIT_USERNAME = login_file.readline().strip('\n')
@@ -108,7 +105,7 @@ REDDIT_PASS = login_file.readline().strip('\n')
 user_agent = login_file.readline().strip('\n')
 if verbose: print 'Logging in as: '+REDDIT_USERNAME
 r = praw.Reddit(user_agent = user_agent,client_id=login_file.readline().strip('\n'),client_secret=login_file.readline().strip('\n'),username=REDDIT_USERNAME,password=REDDIT_PASS)
-if verbose: print '    Login Succesful'
+if verbose: print '    Login Succesful\n'
 subreddit_name=login_file.readline().strip('\n')
 subreddit = r.subreddit(subreddit_name)
 
